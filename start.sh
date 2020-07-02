bd2ae349ab7f69a9dca568c50c29b1009cfce028
ceschenbach <59706628+ceschenbach@users.noreply.github.com>
2020-07-02 15:37:50 -0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+chmod 777 /usr/bin/supervisord
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 echo '<h1>Nginx and PHP-FPM 7.4 inside Docker Container with Ubuntu 20.04 Base Image</h1>' > /var/www/html/index.html
